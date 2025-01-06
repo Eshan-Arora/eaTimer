46e70934b712c85ab7b4f4abb05faba09c62ba23
EshanArora24 <s-arorae@bsd405.org>
2025-01-06 10:47:56 -0800

    Finished basic timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+type TimerState = { state: "NOT_SOLVING"; lastSolveTime: number } | { state: "SOLVING"; startTime: number };
+type Solve = { time: number };
+
+function Timer() {
+  const [timerState, setTimerState] = useState<TimerState>({
+    state: "NOT_SOLVING",
+    lastSolveTime: 0,
+  });
+  const [time, setTime] = useState<number>(Date.now());
+  const [solves, setSolves] = useState<Solve[]>([]);
+  const intervalID = useRef<number | null>(null);
+
+  let timerColor;
+  if (timerState.state === "NOT_SOLVING" || timerState.state === "SOLVING") {
+    timerColor = "black";
+  } else if (timerState.state === "preReady") {
+    timerColor = "red";
+  } else if (timerState.state === "ready") {
+    timerColor = "green";
+  }
+
+  useEffect(() => {
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (event.key === " " && timerState.state === "SOLVING") {
+        stopTimer();
+      }
+      if (event.key === " " && timerState.state === "NOT_SOLVING") {
+        startTimer();
+      }
+    };
+
+    function stopTimer() {
+      if (intervalID.current) {
+        clearInterval(intervalID.current);
+        intervalID.current = null;
+        if (timerState.state === "SOLVING") {
+          const solveTime = time - timerState.startTime;
+          setTimerState({ state: "NOT_SOLVING", lastSolveTime: solveTime });
+          setSolves([...solves, { time: solveTime }]);
+        }
+      }
+    }
+    function startTimer() {
+      if (intervalID.current) {
+        return;
+      }
+      setTimerState({ state: "SOLVING", startTime: Date.now() });
+      intervalID.current = setInterval(() => setTime(Date.now()));
+    }
+
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [timerState, time]);
+
+  function formatNum(num: number): string {
+    return (num / 1000).toFixed(2);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      {timerState.state === "SOLVING" && (
+        <div>
+          <h1 style={{ color: timerColor }}>{formatNum(time - timerState.startTime)}</h1>
+        </div>
+      )}
+      {timerState.state === "NOT_SOLVING" && (
+        <>
+          <h1 style={{ color: timerColor }}>{formatNum(solves.length != 0 ? timerState.lastSolveTime : 0)}</h1>
+          <h2>
+            <button onClick={() => setSolves([])}>Clear Solves</button>
+            <ul>
+              {solves.map((solve) => (
+                <li>{formatNum(solve.time)}</li>
+              ))}
+            </ul>
+          </h2>
+        </>
+      )}
+    </div>
   );
 }
 
-export default App;
+export default Timer;
